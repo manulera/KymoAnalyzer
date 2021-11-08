1c4a53c42ce8f87491f6edef56696e00ea0c122d
Manuel Lera Ramirez <manulera14@gmail.com>
2021-11-08 13:22:40 +0100

    home computer changes

--- analysis/countEvents.m
+++ analysis/countEvents.m
@@ -7,7 +7,8 @@ for cate = unique(this_data.condition_name)'
         clear_duration = sum(this.is_special ==0);
         growing_events = sum(this.is_special ~=2);
         shrinking_events = sum(this.is_special ==2);
-        fprintf('%s\n   nb kymographs:%d\n   growing events: %d\n     > of which clear: %d\n   shrinking events:%d\n',cate,nb_kymos,growing_events,clear_duration,shrinking_events)
+        nb_shrinking_kymos = numel(unique(this.mat_file(this.is_special ==2)));
+        fprintf('%s\n   nb kymographs:%d\n   growing events: %d\n     > of which clear: %d\n   shrinking events:%d\n      > shrinking kymographs:%d\n',cate,nb_kymos,growing_events,clear_duration,shrinking_events,nb_shrinking_kymos)
     end 
 end
 
